4a3b57b03ef1f161f54f89c7b9e794aaa4b36e2d
Ty Jones <whotyjones@gmail.com>
2015-07-30 03:56:14 -0400

    Initial commit

---
+++ TyJonesChat/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using TyJonesChat;
+
+[assembly: OwinStartup( typeof( Startup ) )]
+
+namespace TyJonesChat
+{
+   public class Startup
+   {
+      public void Configuration( IAppBuilder app )
+      {
+         app.Map( "/signalr", map =>
+         {
+            map.UseCors( CorsOptions.AllowAll );
+            var hubConfiguration = new HubConfiguration
+            {
+               EnableJSONP = true
+            };
+            map.RunSignalR( hubConfiguration );
+         } );
+      }
+   }
+}
---
+++ TyJonesChat/TyJonesChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace TyJonesChat
+{
+   public class TyJonesChatHub : Hub
+   {
+      public void Send( string name, string message )
+      {
+         Clients.All.broadcastMessage( name, message );
+      }
+   }
+}
